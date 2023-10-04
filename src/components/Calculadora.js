f395774ec305d59d206a43ec95b701ec6a2be8e8
Henrique Almeida <marceloh500@gmail.com>
2023-10-03 22:48:10 -0300

    Adicionei funcionalidades dos botões

--- src/components/Calculadora.js
+++ src/components/Calculadora.js
@@ -6,18 +6,81 @@ import { useState } from 'react'
 
 function Calculadora(){
 
-    const [valor, setValor] = useState('')
+    const [valor, setValor] = useState([])
     const [resultado, setResultado] = useState(0)
+    const [usarVirgula, setUsarVirgula] = useState(false)
+    const [usarAdicao, setUsarAdicao] = useState(false)
+    const [usarSubtracao, setUsarSubtracao] = useState(false)
+    const [usarMultiplicacao, setUsarMultiplicacao] = useState(false)
+    const [usarDivisao, setUsarDivisao] = useState(false)
+
     
     function adicionarNumero(e){
+        
         setValor('')
         const numero = e.target.value
         setValor(valor + numero)
-        setResultado(valor)
+        setResultado(valor+numero)
+        setUsarAdicao(false)
+        setUsarSubtracao(false)
+        setUsarMultiplicacao(false)
+        setUsarDivisao(false)
     }
     function limparNumeros(){
         setValor('')
     }
+    function deletarNumero(){
+        const novaExpressao = [...valor]
+        const expressaoSemUltimo = novaExpressao.slice(0,-1);
+        setValor(expressaoSemUltimo)
+        setUsarAdicao(false)
+    }
+
+    function resultadoFinal(){
+        const res = eval(valor)
+        setResultado(res)
+         
+    }
+    function adicionarVirgula(){
+        if(!usarVirgula){
+            setValor(valor + ',')
+            setUsarVirgula(true)
+        }
+
+    }
+    function adicionarAdicao(){
+        if(!usarAdicao){
+            setValor(valor + '+')
+            setUsarAdicao(true)
+            setUsarSubtracao(true)
+        }
+    }
+    function adicionarSubtracao(){
+        if(!usarSubtracao){
+            setValor(valor + '-')
+            setUsarSubtracao(true)
+            setUsarAdicao(true)
+        }
+    }
+    function adicionarMultiplicacao(){
+        if(!usarMultiplicacao){
+            setValor(valor + '*')
+            setUsarSubtracao(true)
+            setUsarAdicao(true)
+            setUsarMultiplicacao(true)
+        }
+    }
+    function adicionarDivisao(){
+        if(!usarDivisao){
+            setValor(valor + '/')
+            setUsarSubtracao(true)
+            setUsarAdicao(true)
+            setUsarMultiplicacao(true)
+            setUsarDivisao(true)
+        }
+    }
+    
+    
    
     
    
@@ -39,33 +102,34 @@ function Calculadora(){
                 <p className={styles.valor2}>0</p>
                
                 ):(
-                    <p className={styles.valorResultado}>{`=${valor}`}</p>
-                )
+                    
+                    <p className={styles.valorResultado}>{`=${resultado}`}</p>
+                    )
             }
             </div>
 
             </div>
             <div className={styles.teclado}>
                 <Button texto="C" onClick={()=>limparNumeros()}/>
-                <Button><p className={styles.icone}><FiDelete/></p></Button>
-                <Button texto="%"/>
-                <Button texto="&divide;"/>
+                <Button onClick={deletarNumero}><p className={styles.icone}><FiDelete/></p></Button>
+                <Button texto="%" value="%"/>
+                <Button texto="&divide;" value="/" onClick={adicionarDivisao}/>
                 <Button estilo={styles.buttonNumeros} texto="7" value={7} onClick={adicionarNumero}/>
                 <Button estilo={styles.buttonNumeros} texto="8" value={8} onClick={adicionarNumero}/>
                 <Button estilo={styles.buttonNumeros} texto="9" value={9} onClick={adicionarNumero}/>
-                <Button texto="&#215;"/>
+                <Button texto="&#215;" value="*" onClick={adicionarMultiplicacao}/>
                 <Button estilo={styles.buttonNumeros} texto="4" value={4} onClick={adicionarNumero}/>
                 <Button estilo={styles.buttonNumeros} texto="5" value={5} onClick={adicionarNumero}/>
                 <Button estilo={styles.buttonNumeros} texto="6" value={6} onClick={adicionarNumero}/>
-                <Button texto="-"/>
+                <Button texto="-" value="-" onClick={adicionarSubtracao}/>
                 <Button estilo={styles.buttonNumeros} texto="1" value={1} onClick={adicionarNumero}/>
                 <Button estilo={styles.buttonNumeros} texto="2" value={2} onClick={adicionarNumero}/>
                 <Button estilo={styles.buttonNumeros} texto="3" value={3} onClick={adicionarNumero}/>
-                <Button texto="+" value="+"/>
+                <Button texto="+" value="+" onClick={adicionarAdicao}/>
                 <Button texto="tec"/>
                 <Button estilo={styles.buttonNumeros} texto="0" value={0} onClick={adicionarNumero}/>
-                <Button estilo={styles.buttonNumeros} texto="," value={','} onClick={adicionarNumero}/>
-                <Button estilo={styles.buttonIgualdade}texto="="/>
+                <Button estilo={styles.buttonNumeros} texto="," value={','} onClick={adicionarVirgula}/>
+                <Button estilo={styles.buttonIgualdade}texto="=" onClick={resultadoFinal}/>
                 
 
             </div>
